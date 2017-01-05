6bcd046d41df60f8ac02b118d63573d30d36b6f2
Luiz <argentinaluiz@gmail.com>
2017-01-05 07:35:20 -0200

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import test from './test';
+require('./app.less');
+
+test();
+$(document).ready(function () {
+    $('input:text').val('JQuery funcionando - School of Net');
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+    entry: {
+        app: './index.js',
+        vendor: ['jquery', 'bootstrap'],
+    },
+    output: {
+        filename: '[name].[chunkhash].bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.ejs'
+        }),
+        new InlineManifestWebpackPlugin({
+            name: 'webpackManifest'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: [['es2015',{modules: false}]]
+                }
+            },
+            {
+                test: /\.less$/,
+                loaders: ['style-loader','css-loader','less-loader']
+            },
+            {
+                test: /\.(woff|woff2|ttf|svg|eot)$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 8080
+    },
+    watchOptions: {
+        aggregateTimeout: 300,
+        ignored: /node_modules/
+    },
+    performance: {
+        hints: false
+    },
+    devtool: 'source-map'
+};
+
+/*webpack hash - id da compilacao do pacote
+ chunkhash - id da compilacao do pedaço | do modulo*/
+
+/*grafo - grafico */
